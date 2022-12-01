213b243ffbb54c43c28c616a61412243503804a7
S1N4M <sinamoghimi73@gmail.com>
2022-12-02 01:57:02 +0300

    pack 4 add

---
+++ pack4.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo apt update
+
+sudo apt install ros-noetic-desktop-full -y
+
+sudo apt install ros-noetic-slam-gmapping ros-noetic-ros-numpy ros-noetic-ros-numpy ros-noetic-teleop-twist-keyboard -y
+
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+
+source ~/.bashrc
+
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
+
+sudo rosdep init
+
+rosdep update
+
+
+
