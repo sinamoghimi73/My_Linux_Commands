08ac6b93dfb8e77b5941d468a6d1d40a11682d6a
S1N4M <sinamoghimi73@gmail.com>
2022-12-19 01:08:36 -0800

    add pcl-tools

--- 4_pack4.sh
+++ 4_pack4.sh
@@ -13,7 +13,7 @@ echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 
 source ~/.bashrc
 
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential pcl-tools -y
 
 sudo rosdep init
 
